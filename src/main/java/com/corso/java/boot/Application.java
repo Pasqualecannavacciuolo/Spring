ec3e2573f92ef23dd3e9805064d1d873b334594d
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-13 09:58:08 +0200

    Added first example

--- src/main/java/com/corso/java/boot/Application.java
+++ src/main/java/com/corso/java/boot/Application.java
@@ -1,4 +1,15 @@
 package com.corso.java.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() { return "Ciao Mondo!"; }
 }
