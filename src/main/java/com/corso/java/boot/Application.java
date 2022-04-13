a66ac819a5768043e288455484afc1f8b08e8ae1
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-13 11:18:38 +0200

    Added project structure

--- src/main/java/com/corso/java/boot/Application.java
+++ src/main/java/com/corso/java/boot/Application.java
@@ -9,30 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("api/v1")
+
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/")
-    String home() { return "Ciao Mondo!"; }
-
-    @CrossOrigin(origins = "http://localhost:4200")
-    @RequestMapping("/all")
-    List<User> test1() {
-        List<User> u = new ArrayList<>();
-        u.add(new User("Pasquale"));
-        u.add(new User("Marco"));
-        u.add(new User("Vincenzo"));
-        u.add(new User("Ciro"));
-        return u;
-    }
-
-    @RequestMapping("/user")
-    User test() {
-        return new User("Pasquale");
-    }
 }
